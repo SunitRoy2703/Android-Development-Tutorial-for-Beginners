b32b3fb610c693bf9096fd16073e42ee2c573805
Sunit Roy <2703iamsry@gmail.com>
2021-12-19 00:29:20 +0530

    Added lesson 09

---
+++ 09_MyFirstApp/app/src/main/java/com/sunit/myfirstapp/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.sunit.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
+    }
+}
